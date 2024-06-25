934d18d3ba0b8ceb97899271eac26c671b745fef
Kat Nykiel <katanykiel@gmail.com>
2024-06-25 11:37:58 -0500

    added re search

--- code/expert-workflow.py
+++ code/expert-workflow.py
@@ -7,9 +7,10 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain.prompts import ChatPromptTemplate
+import re
 
 # Create a langchain chat model using the Ollama model
-chat = ChatOllama(model="llama2")
+chat = ChatOllama(model="llama3")
 
 # Define the prompt template for the invoke statement
 prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
@@ -47,5 +48,7 @@ for url in urls:
     retrieval_chain = create_retrieval_chain(retriever, document_chain) 
 
     # Ask a question
-    response = retrieval_chain.invoke({"input": "Which experts are listed in this news article? Please list them as <expert>Expert Name</expert>. andtheir respective institutions as <institution>Institution Name</institution>."})
+    response = retrieval_chain.invoke({"input": "Which experts are listed in this news article? Please list them as <expert>Expert Name</expert>. and their respective institutions as <institution>Institution Name</institution>. There may be more than one, please list all that you find and add newlines between them."})
     print(response["answer"])
+
+ 
